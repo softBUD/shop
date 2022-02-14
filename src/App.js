dae61edfab9483db3be6a1fd5beff73dc08cd005
softBUD <chevimpala21@gmail.com>
2022-02-14 20:30:10 +0900

    0214 input 적용

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
   let [detail,detailState] = useState(['설명1', '설명2' , '설명3']);
   
   let [modal,modalState] = useState(false);
-  let [modalList,modalListState] = useState(0);
+  let [titleNum,titleNumState] = useState(0);
+
+  let [id, idState] = useState("");
+  let [text, textState] = useState(false);
 
   function changeTitle() {
     //state는 등호기호로 직접 조작할 수 없다.
@@ -17,29 +20,36 @@ function App() {
   }
   return (
     <div className="App">
+      {
+        text === true ?
+        <div>{id}</div>
+        : null
+      }
+      <input type="text" onChange={ (e) => {const ids = e.target.value; idState(ids)} }></input>
+      <button onClick= { () => {textState(true)} }>저장</button>
       <div className="shop-nav">
         <div>shop</div>
       </div>
-    {
-     modal === true ?
-     <Modal title={title}></Modal>
-     : null 
-    }
+      {
+      modal === true ?
+      <Modal title={title} titleNum = {titleNum}></Modal>
+      : null 
+      }
 
-    {
-      title.map(function(a,i) {
-        return (
-        <div className="shop-list">
-        <div onClick={ () => {modalState(true)}}>{ a } </div>
-        <button onClick= {changeTitle}>상품수정</button>
-        <hr/>
-        </div>
-        )
-      })
+      {
+        title.map(function(a,i) {
+          return (
+          <div className="shop-list" key = {i}>
+          <div onClick={ () => {modalState(true); titleNumState(i); }}>{ a } </div>
+          <button onClick= {changeTitle}>상품수정</button>
+          <hr/>
+          </div>
+          )
+        })
 
-    }
+      }
 
-    { Forui() }
+      { Forui() }
     </div>
 
   );
@@ -48,7 +58,7 @@ function App() {
 function Modal(props) {
   return (
        <div className='modal'>
-        <h2></h2>
+        <h2>{ props.title[props.titleNum] }</h2>
         <div>상세 날짜</div>
         <div>상세 설명</div>
       </div>
@@ -56,12 +66,14 @@ function Modal(props) {
 }
 
 function Forui() {
-  var array = [];
-  for (var i=0; i<3; i++) {
+  let array = [];
+  for (let i=0; i<3; i++) {
     array.push(<div>반복되는 ui</div>);
   }
 
   return array;
 }
+
+
 
 export default App;
