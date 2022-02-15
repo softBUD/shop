14ecf6e13c8bf39262cc49fe74648af251ae63f7
softBUD <chevimpala21@gmail.com>
2022-02-15 20:46:05 +0900

    0215 bootstrap 적용

--- src/App.js
+++ src/App.js
@@ -1,79 +1,74 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
+import sea from './sea.jpg';
+import forest from './forest.jpg';
+import desert from './desert.jpg';
+import { Navbar,Container,Nav,NavDropdown,Carousel } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  let [title,titleState] = useState(['상품1', '상품2', '상품3']);
-  let [detail,detailState] = useState(['설명1', '설명2' , '설명3']);
-  
-  let [modal,modalState] = useState(false);
-  let [titleNum,titleNumState] = useState(0);
-
-  let [id, idState] = useState("");
-  let [text, textState] = useState(false);
-
-  function changeTitle() {
-    //state는 등호기호로 직접 조작할 수 없다.
-    var newTitle = [...title];
-    newTitle[0] = '새 상품1';
-    titleState( newTitle );
-  }
   return (
-    <div className="App">
-      {
-        text === true ?
-        <div>{id}</div>
-        : null
-      }
-      <input type="text" onChange={ (e) => {const ids = e.target.value; idState(ids)} }></input>
-      <button onClick= { () => {textState(true)} }>저장</button>
-      <div className="shop-nav">
-        <div>shop</div>
-      </div>
-      {
-      modal === true ?
-      <Modal title={title} titleNum = {titleNum}></Modal>
-      : null 
-      }
+  <div className="App">
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home" className='logo'>Travel</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="carFirst"
+          src={sea}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="carSecond"
+          src={forest}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="carThird"
+          src={desert}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
 
-      {
-        title.map(function(a,i) {
-          return (
-          <div className="shop-list" key = {i}>
-          <div onClick={ () => {modalState(true); titleNumState(i); }}>{ a } </div>
-          <button onClick= {changeTitle}>상품수정</button>
-          <hr/>
-          </div>
-          )
-        })
 
-      }
 
-      { Forui() }
-    </div>
+</div>
 
   );
 }
-
-function Modal(props) {
-  return (
-       <div className='modal'>
-        <h2>{ props.title[props.titleNum] }</h2>
-        <div>상세 날짜</div>
-        <div>상세 설명</div>
-      </div>
-  )
-}
-
-function Forui() {
-  let array = [];
-  for (let i=0; i<3; i++) {
-    array.push(<div>반복되는 ui</div>);
-  }
-
-  return array;
-}
-
 
 
 export default App;
