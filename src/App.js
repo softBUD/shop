d169a27ebdb3abad99fc4ae88be5cb063166913c
softBUD <chevimpala21@gmail.com>
2022-02-16 18:02:02 +0900

    0216 css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import sea from './sea.jpg';
-import forest from './forest.jpg';
-import desert from './desert.jpg';
-import { Navbar,Container,Nav,NavDropdown,Carousel } from 'react-bootstrap';
+import lilac from './images/lilac.jpg';
+import fruits from './images/fruits.jpg';
+import leaf from './images/leaf.jpg';
+import bottle1 from './images/bottle1.jpg';
+import bottle2 from './images/bottle2.jpg';
+import bottle3 from './images/bottle3.jpg';
+import { Navbar,Container,Nav,NavDropdown,Carousel,Row,Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -10,13 +13,13 @@ function App() {
   <div className="App">
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home" className='logo'>Travel</Navbar.Brand>
+        <Navbar.Brand href="#home" className='logo'>Cosme</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#link">product</Nav.Link>
+            <NavDropdown title="Log in" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -31,38 +34,56 @@ function App() {
       <Carousel.Item>
         <img
           className="carFirst"
-          src={sea}
+          src={lilac}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3 className='slideTitle' id='slideTitleFirst'>Floral</h3>
+          <p id='slideTextFirst'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="carSecond"
-          src={forest}
+          src={fruits}
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3 className='slideTitle' id='slideTitleSecond'>Fruits</h3>
+          <p id='slideTextSecond'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="carThird"
-          src={desert}
+          src={leaf}
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <h3 className='slideTitle' id='slideTitleThird'>Nature</h3>
+          <p id='slideTextThird'>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
+    <Container className='listContainer'>
+      <Row className='listWrap'>
+        <Col className='listContent'>
+          <img src={bottle1} alt="bottle1" className='listImg'/>
+          <div>nora</div>
+          <div>35000</div>
+        </Col>
+        <Col className='listContent'>
+        <img src={bottle2} alt="bottle2" className='listImg'/>
+        <div>scruffy</div>
+        <div>20000</div>
+        </Col>
+        <Col className='listContent'>
+          <img src={bottle3} alt="bottle3" className='listImg'/>
+          <div>hella</div>
+          <div>18000</div>
+        </Col>
+      </Row>
+    </Container>
 
 
 </div>
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 0,
+        title : "nora",
+        price : "35000 "
+    },
+    {
+        id: 1,
+        title : "scruffy",
+        price : "20000"
+    },
+    {
+        id: 2,
+        title : "hella",
+        price : "35000 "
+    }
+]
