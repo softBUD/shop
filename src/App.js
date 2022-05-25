b4efc80f4ef4c57e529b803ea53afade51de9054
softBUD <chevimpala21@gmail.com>
2022-05-25 12:54:02 +0900

    ⚡️ app.js lazy 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { useState,Suspense,lazy} from 'react';
 import {useDispatch} from 'react-redux';
 import { Route, BrowserRouter,withRouter, Switch} from 'react-router-dom';
-import Cart from './views/cart.js';
-import Detail from './views/detail.js';
-import Login from './views/login.js';
-import Signup from './views/sign.js';
 import Auth from './hoc/auth.js';
-import Upload from './views/upload.js';
-import Landing from './views/landing.js';
 import Footer from './views/footer.js';
 import './App.css';
 
@@ -19,7 +13,6 @@ function App() {
   const login = lazy(()=>import('./views/login'));
   const sign = lazy(()=>import('./views/sign'));
   const upload = lazy(()=>import('./views/upload'));
-  const footer = lazy(()=>import('./views/footer'));
 
 
   const dispatch = useDispatch();
@@ -35,12 +28,12 @@ function App() {
   <BrowserRouter>
     <Suspense fallback={<div>페이지 로딩중...</div>}>
     <Switch>
-      <Route path='/user/login' component={Auth(Login, false)} />
-      <Route path="/user/signup" component={Auth(Signup,false)}></Route>
-      <Route path="/upload" component={Auth(Upload,true)}/>
-      <Route path="/api/cart" component={Auth(Cart,true)}/>
-      <Route path="/api/product/:productId" component={Auth(Detail,null)}></Route>
-      <Route path="/" component={Auth(Landing, null)}/>
+      <Route path='/user/login' component={Auth(login, false)} />
+      <Route path="/user/signup" component={Auth(sign,false)}></Route>
+      <Route path="/upload" component={Auth(upload,true)}/>
+      <Route path="/api/cart" component={Auth(cart,true)}/>
+      <Route path="/api/product/:productId" component={Auth(detail,null)}></Route>
+      <Route path="/" component={Auth(landing, null)}/>
     </Switch>
     <Footer></Footer>
     </Suspense>
