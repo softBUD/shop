9073840e37d22c7045e1c6cce8a3b7aa6704f3af
softBUD <chevimpala21@gmail.com>
2022-05-25 13:53:11 +0900

    🔥 lazy 미적용시 성능 비교

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState,Suspense,lazy} from 'react';
+import React, { useState,Suspense} from 'react';
 import {useDispatch} from 'react-redux';
 import { Route, BrowserRouter,withRouter, Switch} from 'react-router-dom';
+import Cart from './views/cart.js';
+import Detail from './views/detail.js';
+import Login from './views/login.js';
+import Signup from './views/sign.js';
 import Auth from './hoc/auth.js';
+import Upload from './views/upload.js';
+import Landing from './views/landing.js';
 import Footer from './views/footer.js';
 import './App.css';
 
 
 function App() {
-  const landing = lazy(()=>import('./views/landing'));
-  const cart = lazy(()=>import('./views/cart'));
-  const detail = lazy(()=>import('./views/detail'));
-  const login = lazy(()=>import('./views/login'));
-  const sign = lazy(()=>import('./views/sign'));
-  const upload = lazy(()=>import('./views/upload'));
+  
 
 
   const dispatch = useDispatch();
@@ -28,12 +29,12 @@ function App() {
   <BrowserRouter>
     <Suspense fallback={<div>페이지 로딩중...</div>}>
     <Switch>
-      <Route path='/user/login' component={Auth(login, false)} />
-      <Route path="/user/signup" component={Auth(sign,false)}></Route>
-      <Route path="/upload" component={Auth(upload,true)}/>
-      <Route path="/api/cart" component={Auth(cart,true)}/>
-      <Route path="/api/product/:productId" component={Auth(detail,null)}></Route>
-      <Route path="/" component={Auth(landing, null)}/>
+      <Route path='/user/login' component={Auth(Login, false)} />
+      <Route path="/user/signup" component={Auth(Signup,false)}></Route>
+      <Route path="/upload" component={Auth(Upload,true)}/>
+      <Route path="/api/cart" component={Auth(Cart,true)}/>
+      <Route path="/api/product/:productId" component={Auth(Detail,null)}></Route>
+      <Route path="/" component={Auth(Landing, null)}/>
     </Switch>
     <Footer></Footer>
     </Suspense>
